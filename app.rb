9449396625fd376cb9f8e5b7ec27d001653ede3a
Sarah Power <sarahjonipower0@gmail.com>
2020-12-07 17:31:33 +0000

    cat image added

--- app.rb
+++ app.rb
@@ -19,10 +19,8 @@ get '/anotherone' do
 end
 
 get '/cat' do
-  div {
-    border: dashed red
-  }
-  "<div>
-    <img src='https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d'
+  "<div style='border: 3px dashed red'>
+    <img src='https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d'>
    </div>"
 end
+
